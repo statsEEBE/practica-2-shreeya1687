0fa2e49034975119f3ecb3b163a9526c39f378db
shreeya.baniya <shreeya.baniya@auleseebe.upc.edu>
2025-02-25 09:44:59 +0100

    sol 1

--- Problema1.R
+++ Problema1.R
@@ -1,1 +1,16 @@
 #Codigo para problema 1
+
+#coeficient de determinacio: R^2
+Rsqrt<- 19.23
+
+#coeficient de correlació: R
+sqrt(19.23)
+
+#c: m
+m<-0.06576
+
+#d: x=647.1
+m<- 0.06576
+b<- 11.79755
+
+m*647.1+b
